12b0465b0b77252751f719858397068bfd6b9f2d
Joshdroid250 <gutierrez25g@gmail.com>
2022-04-19 11:49:52 -0600

    Actualizando Interface

--- app/src/main/java/com/example/peliculas/PrincipalMenu.kt
+++ app/src/main/java/com/example/peliculas/PrincipalMenu.kt
@@ -5,55 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.peliculas.databinding.FragmentPrincipalMenuBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PrincipalMenu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrincipalMenu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var fragBinding: FragmentPrincipalMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_principal_menu, container, false)
+        fragBinding = FragmentPrincipalMenuBinding.inflate(layoutInflater)
+        return fragBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrincipalMenu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrincipalMenu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragBinding.btnVerGenero.setOnClickListener {
+
+        }
+        fragBinding.addGenero.setOnClickListener {
+            it.findNavController().navigate(R.id.addGenero)
+        }
     }
 }
